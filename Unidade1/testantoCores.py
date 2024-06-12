1721fb456528f40765cff2b936889d96794220c3
Antonio Vieira S. Neto <antonionetoacademic@gmail.com>
2024-06-12 18:37:57 -0300

    Delete Unidade1 directory

--- Unidade1/testantoCores.py
+++
@@ -1,7 +0,0 @@
-print('\033[1:7:94:40m 1-OLA MUNDO \033[m')
-print('\033[1:7:96:40m 2-OLA MUNDO \033[m')
-print('\033[1:7:97:40m 3-OLA MUNDO \033[m')
-print('\033[1:7:95:40m 4-OLA MUNDO \033[m')
-print('\033[1:7:93:40m 5-OLA MUNDO \033[m')
-print('\033[1:7:91:40m 6-OLA MUNDO \033[m')
-print('\033[1:7:92:40m 7-OLA MUNDO \033[m')
